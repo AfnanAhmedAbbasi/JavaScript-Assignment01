5957950fbf3740149ea6cb800020ba28a6cd3932
Afnan Abbasi <afnanabbasi0k@gmail>
2024-03-12 16:19:22 +0500

    JavaScript-Assignment01

---
+++ TesTing.js
@@ -0,0 +1,12 @@
+//Task 01
+var q1$_ = "Error! please enter a valid password"
+alert(q1$_)
+//Task 02
+var q2$_ = "Welcome to JS Land..."
+var q3$_ = "Coding!"
+alert(q2$_ +"\n"+ q3$_)
+//Task 03
+var q4$_ = "Welcome to JS Land.."
+window.alert(q4$_)
+//Task 04
+console.log("Hello... I can run JS through my web browser's console")
